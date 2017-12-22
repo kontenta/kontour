66fcf2d18bccfc537c6ff4327cc1f9c0b22e7c2b
Erik Lindegren <erik@epineer.se>
2017-12-22 15:53:00 +0100

    Set up base test cases

---
+++ tests/IntegrationTest.php
@@ -0,0 +1,10 @@
+<?php
+
+namespace Erik\AdminManagerImplementation\Tests;
+
+use Orcehstra\Testbench\TestCase;
+
+abstract class IntegrationTest extends TestCase
+{
+
+}
---
+++ tests/UnitTest.php
@@ -0,0 +1,10 @@
+<?php
+
+namespace Erik\AdminManagerImplementation\Tests;
+
+use PHPUnit\Framework\TestCase;
+
+abstract class UnitTest extends TestCase
+{
+
+}
