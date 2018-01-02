a962450aa8da73cde9ca1abfead552715a037641
Björn Nilsved <nilsved@gmail.com>
2018-01-02 17:06:20 +0100

    Register the package service provider for tests

--- tests/IntegrationTest.php
+++ tests/IntegrationTest.php
@@ -2,9 +2,20 @@
 
 namespace Erik\AdminManagerImplementation\Tests;
 
-use Orcehstra\Testbench\TestCase;
+use Orchestra\Testbench\TestCase;
 
 abstract class IntegrationTest extends TestCase
 {
-
+    /**
+     * Get the service providers for the package.
+     *
+     * @param  \Illuminate\Foundation\Application $app
+     * @return array
+     */
+    protected function getPackageProviders($app)
+    {
+        return [
+            'Erik\AdminManagerImplementation\AdminManagerServiceProvider',
+        ];
+    }
 }
