4ae2efa1eb9671a7df9599837ff0477ba12eafc8
Björn Nilsved <nilsved@gmail.com>
2018-01-03 14:14:12 +0100

    Set up testing with userland packages

---
+++ tests/Feature/Fakes/UserlandServiceProvider.php
@@ -0,0 +1,24 @@
+<?php
+
+namespace Erik\AdminManagerImplementation\Tests\Feature\Fakes;
+
+use Illuminate\Support\ServiceProvider;
+
+class UserlandServiceProvider extends ServiceProvider
+{
+    /**
+     * Register bindings in the container.
+     */
+    public function register()
+    {
+        //
+    }
+
+    /**
+     * Bootstrap any application services.
+     */
+    public function boot()
+    {
+        //
+    }
+}
---
+++ tests/Feature/UserlandServiceProviderTest.php
@@ -0,0 +1,12 @@
+<?php
+
+namespace Erik\AdminManagerImplementation\Tests\Feature;
+
+use Erik\AdminManagerImplementation\Tests\UserlandAdminToolTest;
+
+class UserlandServiceProviderTest extends UserlandAdminToolTest
+{
+    public function test_routes_are_registered() {
+        //TODO: test that userland routes exist
+    }
+}
