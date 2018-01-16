b2c66f2986f0ac927f9f0e9e9107ea8a12712d2d
Björn Nilsved <nilsved@gmail.com>
2018-01-16 14:24:40 +0100

    Test guest redirect

---
+++ tests/Feature/AuthenticationTest.php
@@ -0,0 +1,33 @@
+<?php
+
+namespace Erik\AdminManagerImplementation\Tests\Feature;
+
+use Erik\AdminManager\Contracts\AdminRouteManager;
+use Erik\AdminManagerImplementation\Tests\IntegrationTest;
+use Illuminate\Foundation\Auth\User;
+
+class AuthenticationTest extends IntegrationTest
+{
+    /**
+     * @var User
+     */
+    private $user;
+
+    public function setUp()
+    {
+        parent::setUp();
+        $this->prepareDatabase();
+        $this->user = factory(User::class)->create();
+    }
+
+    public function test_guest_is_redirected_to_login()
+    {
+        /**
+         * @var $routeManager AdminRouteManager
+         */
+        $routeManager = $this->app->make(AdminRouteManager::class);
+        $response = $this->get($routeManager->indexUrl());
+
+        $response->assertRedirect($routeManager->loginUrl());
+    }
+}
