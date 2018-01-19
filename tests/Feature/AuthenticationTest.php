3637939e1b40fe9aa0a6402d9fb57b8781e37fca
Björn Nilsved <nilsved@gmail.com>
2018-01-19 18:56:34 +0100

    Improve todo with links to docs

--- tests/Feature/AuthenticationTest.php
+++ tests/Feature/AuthenticationTest.php
@@ -33,5 +33,7 @@ class AuthenticationTest extends IntegrationTest
 
     // TODO: test_ajax_guest_is_not_redirected
 
-    // TODO: test login procedure - with Dusk? https://laravel.com/docs/5.5/dusk
+    // TODO: test login procedure - with Dusk?
+    // https://github.com/orchestral/testbench-dusk
+    // https://laravel.com/docs/5.5/dusk
 }
