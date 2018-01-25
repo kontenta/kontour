42b01dfdb0648a69315d7a13437425c086d5d547
Björn Nilsved <nilsved@gmail.com>
2018-01-25 15:05:43 +0100

    Add login post route

--- routes/auth.php
+++ routes/auth.php
@@ -3,6 +3,7 @@ use Illuminate\Support\Facades\Route;
 
 Route::middleware('web')->namespace('Erik\AdminManagerImplementation\Http\Controllers\Auth')->group(function() {
     Route::get('login', 'LoginController@showLoginForm')->name('admin.login');
+    Route::post('login', 'LoginController@login');
     //TODO: implement password reset
     Route::get('password/reset', function() {})->name('admin.password.request');
 });
