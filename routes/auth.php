9316143f15ec2e1e83eb0a19496052f690499250
Björn Nilsved <nilsved@gmail.com>
2018-02-02 15:00:10 +0100

    Add stub routes for password reset

--- routes/auth.php
+++ routes/auth.php
@@ -5,6 +5,9 @@ Route::middleware('web')->namespace('Erik\AdminManagerImplementation\Http\Contro
     Route::get('login', 'LoginController@showLoginForm')->name('admin.login');
     Route::post('login', 'LoginController@login');
     Route::post('logout', 'LoginController@logout')->name('admin.logout');
-    //TODO: implement password reset
-    Route::get('password/reset', function() {})->name('admin.password.request');
+
+    Route::get('password/reset', function() { return false; })->name('admin.password.request');
+    Route::post('password/email', function() { return false; })->name('admin.password.email');
+    Route::get('password/reset/{token}', function() { return false; })->name('admin.password.reset');
+    Route::post('password/reset', function() { return false; });
 });
