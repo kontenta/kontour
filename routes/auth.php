30d24e1e3608abcf3a782e823d4f6e87ffd52a49
Björn Nilsved <nilsved@gmail.com>
2018-05-03 16:37:18 +0200

    Remove middleware that is already added by trait

--- routes/auth.php
+++ routes/auth.php
@@ -2,7 +2,7 @@
 
 use Illuminate\Support\Facades\Route;
 
-Route::middleware('web')->namespace('Erik\AdminManagerImplementation\Http\Controllers\Auth')->group(function () {
+Route::namespace('Erik\AdminManagerImplementation\Http\Controllers\Auth')->group(function () {
     Route::get('login', 'LoginController@showLoginForm')->name('admin.login');
     Route::post('login', 'LoginController@login');
     Route::post('logout', 'LoginController@logout')->name('admin.logout');
