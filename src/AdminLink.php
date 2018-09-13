6023f70930eb05bd7a3829eb06739e2cf80242cd
Björn Nilsved <nilsved@gmail.com>
2018-09-13 16:28:09 +0200

    Allow null description

--- src/AdminLink.php
+++ src/AdminLink.php
@@ -14,7 +14,7 @@ class AdminLink implements AdminLinkContract, AuthorizesWithAbilityContract
     protected $name;
     protected $description;
 
-    public function __construct(string $url, string $name, string $description = '')
+    public function __construct(string $url, string $name, string $description = null)
     {
         $this->url = $url;
         $this->name = $name;
@@ -31,7 +31,7 @@ class AdminLink implements AdminLinkContract, AuthorizesWithAbilityContract
         return $this->name;
     }
 
-    public function getDescription(): string
+    public function getDescription(): ?string
     {
         return $this->description;
     }
