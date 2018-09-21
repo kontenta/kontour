5e378901c3a3e87e205472dd7c1a1f7f11f4b295
Björn Nilsved <nilsved@gmail.com>
2018-09-21 10:11:30 +0200

    Use AdminUser contract

--- tests/Fakes/UrlVisit.php
+++ tests/Fakes/UrlVisit.php
@@ -4,16 +4,16 @@ namespace Kontenta\Kontour\Tests\Fakes;
 
 use Kontenta\Kontour\Contracts\UrlVisit as UrlVisitContract;
 use Kontenta\Kontour\Contracts\AdminLink as AdminLinkContract;
-use Illuminate\Contracts\Auth\Access\Authorizable;
+use Kontenta\Kontour\Contracts\AdminUser;
 
 class UrlVisit implements UrlVisitContract
 {
     public function getLink(): AdminLinkContract
     {
-        return new AdminLink();        
+        return new AdminLink();
     }
 
-    public function getUser(): Authorizable
+    public function getUser(): AdminUser
     {
         return new Illuminate\Auth\GenericUser();
     }
