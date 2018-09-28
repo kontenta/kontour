bc7f1b4126b5f7bf5f1ac12b624667fcf83678aa
Erik Lindegren <erik@epineer.se>
2018-09-28 15:49:42 +0200

    Add interface for ItemHistoryWidget

---
+++ src/Contracts/ItemHistoryWidget.php
@@ -0,0 +1,14 @@
+<?php
+
+namespace Kontenta\Kontour\Contracts;
+
+interface ItemHistoryWidget extends AdminWidget
+{
+    /**
+     * Add an entry to an item's history
+     * @param AdminUser $user
+     * @param \DateTimeImmutable $datetime
+     * @param string $action
+     */
+    public function addEntry(AdminUser $user, \DateTimeImmutable $datetime, string $action = null ): ItemHistoryWidget;
+}
