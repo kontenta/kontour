5b67abd38cc025dabfa5ff3b6c458f8f8036c040
Björn Nilsved <nilsved@gmail.com>
2018-10-16 09:43:43 +0200

    Add methods for urls and named routes

--- src/Concerns/RegistersMenuWidgetLinks.php
+++ src/Concerns/RegistersMenuWidgetLinks.php
@@ -2,12 +2,26 @@
 
 namespace Kontenta\Kontour\Concerns;
 
-use Kontenta\Kontour\Contracts\MenuWidget;
+use Kontenta\Kontour\AdminLink as UrlAdminLink;
 use Kontenta\Kontour\Contracts\AdminLink;
+use Kontenta\Kontour\Contracts\MenuWidget;
+use Kontenta\Kontour\RouteAdminLink;
 
 trait RegistersMenuWidgetLinks
 {
-    public function addMenuWidgetLink(AdminLink $link, string $desiredHeading = null)
+    public function addMenuWidgetUrl(string $url, string $name, string $description = null, string $desiredHeading = null)
+    {
+        $link = new UrlAdminLink($url, $name, $description);
+        $this->addMenuWidgetAdminLink($link, $desiredHeading);
+    }
+
+    public function addMenuWidgetRoute(string $routeName, string $name, string $description = null, string $desiredHeading = null)
+    {
+        $link = new RouteAdminLink($routeName, $name, $description);
+        $this->addMenuWidgetAdminLink($link, $desiredHeading);
+    }
+
+    public function addMenuWidgetAdminLink(AdminLink $link, string $desiredHeading = null)
     {
         $this->resolveMenuWidget()->addLink($link, $desiredHeading);
     }
