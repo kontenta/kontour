7258fc152856449567016039a2918757e250921c
Björn Nilsved <nilsved@gmail.com>
2018-10-17 13:02:38 +0200

    Add route parameters to RouteAdminLink

--- src/RouteAdminLink.php
+++ src/RouteAdminLink.php
@@ -2,22 +2,27 @@
 
 namespace Kontenta\Kontour;
 
-use Illuminate\Support\Facades\Route;
 use Illuminate\Support\Facades\URL;
 
 class RouteAdminLink extends AdminLink
 {
     protected $routeName;
+    protected $routeParameters;
 
-    public function __construct(string $name, string $routeName, string $description = null)
+    public function __construct(string $name, string $routeName, $routeParameters = [], string $description = null)
     {
         $this->name = $name;
         $this->routeName = $routeName;
+        $this->routeParameters = $routeParameters;
         $this->withDescription($description);
     }
 
     public function getUrl(): ?string
     {
-        return Route::has($this->routeName) ? URL::route($this->routeName) : null;
+        try {
+            return URL::route($this->routeName, $this->routeParameters);
+        } catch (\Exception $e) {
+            return null;
+        }
     }
 }
--- tests/Feature/RouteAdminLinkTest.php
+++ tests/Feature/RouteAdminLinkTest.php
@@ -7,11 +7,39 @@ use Kontenta\Kontour\Tests\IntegrationTest;
 
 class RouteAdminLinkTest extends IntegrationTest
 {
+    /**
+     * Configure the environment.
+     *
+     * @param  \Illuminate\Foundation\Application $app
+     * @return void
+     */
+    protected function getEnvironmentSetUp($app)
+    {
+        // Test with Laravel's default password reset config
+        // This is to have the route kontour.password.reset requiring parameter token available
+        $app['config']->set('kontour.passwords', 'users');
+        parent::getEnvironmentSetUp($app);
+    }
+
     public function test_url_is_null_when_route_does_not_exist()
     {
-        $link = new RouteAdminLink('Hej', 'not.existing', '"Hejsanhejsan"');
+        $link = new RouteAdminLink('Gone', 'not.existing');
 
         $this->assertNull($link->getUrl());
-        $this->assertEquals('<a title="&quot;Hejsanhejsan&quot;">Hej</a>', $link->toHtml());
+        $this->assertEquals('<a >Gone</a>', $link->toHtml());
+    }
+
+    public function test_url_is_null_when_parameters_missing()
+    {
+        $link = new RouteAdminLink('Reset password', 'kontour.password.reset');
+
+        $this->assertNull($link->getUrl());
+    }
+
+    public function test_route_parameters_can_be_added_in_constructor()
+    {
+        $link = new RouteAdminLink('Reset password', 'kontour.password.reset', ["token" => 'abc']);
+
+        $this->assertEquals(route('kontour.password.reset', ["token" => 'abc']), $link->getUrl());
     }
 }
