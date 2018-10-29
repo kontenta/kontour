ff8ec6d91fe4b413c2f05085ef774126122df303
Björn Nilsved <nilsved@gmail.com>
2018-10-29 15:18:41 +0100

    Add basic Gate tests

--- tests/Feature/AuthorizesWithAbilityTest.php
+++ tests/Feature/AuthorizesWithAbilityTest.php
@@ -2,9 +2,10 @@
 
 namespace Kontenta\Kontour\Tests\Feature;
 
+use Illuminate\Support\Facades\Gate;
 use Kontenta\Kontour\AdminLink;
-use Kontenta\Kontour\Tests\IntegrationTest;
 use Kontenta\Kontour\Tests\Feature\Fakes\User;
+use Kontenta\Kontour\Tests\IntegrationTest;
 
 class AuthorizesWithAbilityTest extends IntegrationTest
 {
@@ -20,7 +21,25 @@ class AuthorizesWithAbilityTest extends IntegrationTest
         $this->user = factory(User::class)->create();
     }
 
-    public function test_can_be_serialized_and_deserialized() {
+    public function test_authorizing_with_gate()
+    {
+        Gate::define('testGate', function ($user) {
+            return true;
+        });
+        $link = AdminLink::create('Reset password', 'http://test.com')->registerAbilityForAuthorization('testGate');
+
+        $this->assertTrue($link->isAuthorized($this->user));
+    }
+
+    public function test_not_authorized_without_user()
+    {
+        $link = AdminLink::create('Reset password', 'http://test.com')->registerAbilityForAuthorization('testGate');
+
+        $this->assertFalse($link->isAuthorized());
+    }
+
+    public function test_can_be_serialized_and_deserialized()
+    {
         $link = AdminLink::create('Reset password', 'http://test.com');
 
         $serializedLink = serialize($link);
