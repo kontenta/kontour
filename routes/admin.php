919a62f36249749282980aba9da4c2d024f945c4
Björn Nilsved <nilsved@gmail.com>
2018-10-31 14:12:05 +0100

    Format code

--- routes/admin.php
+++ routes/admin.php
@@ -3,7 +3,7 @@
 use Illuminate\Support\Facades\Route;
 
 Route::namespace('Kontenta\Kontour\Http\Controllers')->group(function () {
-    if(!Route::has('kontour.index')) {
+    if (!Route::has('kontour.index')) {
         Route::get('/', 'IndexController')->name('kontour.index');
     }
 });
