d431a5885e4b001bc97b9a8327d4e48edded1f63
Björn Nilsved <nilsved@gmail.com>
2018-11-05 11:20:55 +0100

    Make aria-label translateable

--- resources/views/widgets/crumbtrail.blade.php
+++ resources/views/widgets/crumbtrail.blade.php
@@ -1,4 +1,4 @@
-<nav aria-label="Crumb trail" data-kontour-widget="crumbtrail">
+<nav aria-label="{{ __('Crumb trail') }}" data-kontour-widget="crumbtrail">
   <ol>
     @foreach($links as $link)
       <li{!! url()->full() == $link->getUrl()?' aria-current="page"':'' !!}>{{ $link }}</li>
