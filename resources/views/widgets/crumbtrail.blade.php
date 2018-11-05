ad2ad047cad9692208d0e27dc3b7aafabf99aa47
Björn Nilsved <nilsved@gmail.com>
2018-11-05 11:23:03 +0100

    Format code

--- resources/views/widgets/crumbtrail.blade.php
+++ resources/views/widgets/crumbtrail.blade.php
@@ -1,7 +1,7 @@
 <nav aria-label="{{ __('Crumb trail') }}" data-kontour-widget="crumbtrail">
   <ol>
     @foreach($links as $link)
-      <li{!! url()->full() == $link->getUrl()?' aria-current="page"':'' !!}>{{ $link }}</li>
+      <li{!! url()->full() == $link->getUrl() ? ' aria-current="page"' : '' !!}>{{ $link }}</li>
     @endforeach
   </ol>
 </nav>
