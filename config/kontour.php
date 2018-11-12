65af5133d6d08a7316d9e8647e1d31b1cf1601bf
Björn Nilsved <nilsved@gmail.com>
2018-11-12 11:29:37 +0100

    Add title to config

--- config/kontour.php
+++ config/kontour.php
@@ -14,6 +14,20 @@ return [
     |
     */
 
+    'title' => 'Kontour',
+
+    /*
+    |--------------------------------------------------------------------------
+    | Admin Authentication Guard
+    |--------------------------------------------------------------------------
+    |
+    | Name of the default Guard used for admin tools.
+    |
+    | If you've added a configuration for a separate Guard for admin pages in
+    | your app's config/auth.php you should set that guard's name here.
+    |
+    */
+
     'guard' => 'web',
 
     /*
