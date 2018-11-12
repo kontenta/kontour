063283678e278ffe28152b62436401a4cce3273b
Björn Nilsved <nilsved@gmail.com>
2018-11-12 12:16:32 +0100

    Use default title from config

--- resources/views/layouts/html.blade.php
+++ resources/views/layouts/html.blade.php
@@ -5,7 +5,7 @@
   <meta http-equiv="X-UA-Compatible" content="IE=edge">
   <meta name="viewport" content="width=device-width, initial-scale=1">
 
-  <title>@yield('title', 'Admin')</title>
+  <title>@yield('title', config('kontour.title'))</title>
 
   @stack('meta')
 
