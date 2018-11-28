fe75d907a22b4280761bf3eed4ba022c9979a6b3
Björn Nilsved <nilsved@gmail.com>
2018-11-28 15:00:41 +0100

    Remove the bootstrap html

--- resources/views/auth/login.blade.php
+++ resources/views/auth/login.blade.php
@@ -4,29 +4,19 @@
 @extends($view_manager->layout())
 
 @section($view_manager->mainSection())
-<div class="container">
-    <div class="row justify-content-center">
-        <div class="col-md-8">
-            <div class="card">
-              <div class="card-header">{{ __('Login') }}</div>
-                <div class="card-body">
-                    <form method="POST" action="{{ route('kontour.login') }}">
-                      @csrf
-                      @include('kontour::forms.input', ['name' => 'email', 'type' => 'email', 'controlAttributes' => ['required']])
-                      @include('kontour::forms.input', ['name' => 'password', 'type' => 'password', 'controlAttributes' => ['required']])
-                      @include('kontour::forms.checkbox', ['name' => 'remember', 'label' => __('Remember Me')])
-                      <button type="submit" class="btn btn-primary">
-                        {{ __('Login') }}
-                      </button>
-                      @if($route_manager->passwordResetUrl())
-                        <a class="btn btn-link" href="{{ $route_manager->passwordResetUrl() }}">
-                          {{ __('Forgot Your Password?') }}
-                        </a>
-                      @endif
-                    </form>
-                </div>
-            </div>
-        </div>
-    </div>
-</div>
+  <header>{{ __('Login') }}</header>
+  <form method="POST" action="{{ route('kontour.login') }}">
+    @csrf
+    @include('kontour::forms.input', ['name' => 'email', 'type' => 'email', 'controlAttributes' => ['required']])
+    @include('kontour::forms.input', ['name' => 'password', 'type' => 'password', 'controlAttributes' => ['required']])
+    @include('kontour::forms.checkbox', ['name' => 'remember', 'label' => __('Remember Me')])
+    <button type="submit">
+      {{ __('Login') }}
+    </button>
+    @if($route_manager->passwordResetUrl())
+      <a href="{{ $route_manager->passwordResetUrl() }}">
+        {{ __('Forgot Your Password?') }}
+      </a>
+    @endif
+  </form>
 @endsection
