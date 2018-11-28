e564083b9da790ef28b694a1174dd0b3307421c4
Björn Nilsved <nilsved@gmail.com>
2018-11-28 15:25:11 +0100

    Add todo

--- resources/views/auth/passwords/email.blade.php
+++ resources/views/auth/passwords/email.blade.php
@@ -5,10 +5,11 @@
 @section($view_manager->mainSection())
   <header>{{ __('Reset Password') }}</header>
 
+  {{-- TODO: Use the MessageWidget instead of hard coded alert from session --}}
   @if (session('status'))
-      <div class="alert alert-success" role="alert">
-          {{ session('status') }}
-      </div>
+    <div class="alert alert-success" role="alert">
+      {{ session('status') }}
+    </div>
   @endif
 
   <form method="POST" action="{{ route('kontour.password.email') }}">
