ab900d985817d4843d323e1c2bd0d3ec6116fc27
Björn Nilsved <nilsved@gmail.com>
2018-12-05 13:17:51 +0100

    Add hidden empty input to indicate presence of multi-select array fields

--- resources/views/forms/multiselect.blade.php
+++ resources/views/forms/multiselect.blade.php
@@ -1,6 +1,7 @@
 <div data-selected-options="{{ implode(' ', $selected = collect(old($name, $selected ?? $model[$name] ?? []))->map(function($item) { return strval($item instanceof Illuminate\Database\Eloquent\Model ? $item->getKey() : $item); })->all()) }}">
   @include('kontour::forms.label', ['controlId' => $controlId = $controlId ?? (($idPrefix ?? '') . $name)])
   <div>
+    <input type="hidden" name="{{ $name }}" value="">
     <select multiple
       @include('kontour::forms.partials.inputAttributes', ['name' => $name . '[]', 'errorsId' => $errorsId = $controlId . ($errorsSuffix ?? 'Errors')])
     >
