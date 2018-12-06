e55ea867154ee72c835baa5d37763e452fc017fc
Björn Nilsved <nilsved@gmail.com>
2018-12-06 17:27:00 +0100

    Make the password reset link absolute
    This adds full domain name so that link works in email

--- src/Auth/Notifications/ResetPassword.php
+++ src/Auth/Notifications/ResetPassword.php
@@ -16,7 +16,7 @@ class ResetPassword extends \Illuminate\Auth\Notifications\ResetPassword
     {
         return (new MailMessage)
             ->line('You are receiving this email because we received a password reset request for your account.')
-            ->action('Reset Password', route('kontour.password.reset', $this->token, false))
+            ->action('Reset Password', route('kontour.password.reset', $this->token))
             ->line('If you did not request a password reset, no further action is required.');
     }
 }
