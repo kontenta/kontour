ccf6a25dbed5c13081b21988e62f131a1ab978b5
Björn Nilsved <nilsved@gmail.com>
2018-12-07 16:43:11 +0100

    Use ResolvesAdminUser trait

--- src/Widgets/UserAccountWidget.php
+++ src/Widgets/UserAccountWidget.php
@@ -6,13 +6,15 @@ use Illuminate\Contracts\Auth\Access\Authorizable;
 use Illuminate\Support\Facades\Auth;
 use Illuminate\Support\Facades\View;
 use Kontenta\Kontour\Contracts\UserAccountWidget as UserAccountWidgetContract;
+use Kontenta\Kontour\Concerns\ResolvesAdminUser;
 
 class UserAccountWidget implements UserAccountWidgetContract
 {
+    use ResolvesAdminUser;
+
     public function toHtml()
     {
-        $user = Auth::guard(config('kontour.guard'))->user();
-        return View::make('kontour::widgets.userAccount', compact('user'))->render();
+        return View::make('kontour::widgets.userAccount', ['user' => $this->user()])->render();
     }
 
     public function isAuthorized(Authorizable $user = null): bool
