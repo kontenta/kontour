70af9cb5fa4c1fef268c588e61f4b2bef26d0c61
Björn Nilsved <nilsved@gmail.com>
2018-12-11 10:28:25 +0100

    Test default label tag

--- tests/Feature/FormViewTests/LabelTest.php
+++ tests/Feature/FormViewTests/LabelTest.php
@@ -8,6 +8,13 @@ use Kontenta\Kontour\Tests\IntegrationTest;
 
 class LabelTest extends IntegrationTest
 {
+    public function test_default_label_tag()
+    {
+        $output = View::make('kontour::forms.label', ['name' => 'test'])->render();
+
+        $this->assertRegExp('/<label[\S\s]*>[\S\s]*<\/label>/', $output);
+    }
+
     public function test_generated_label_is_humanized()
     {
         $output = View::make('kontour::forms.label', ['name' => 'test_input'])->render();
