acb76a3f89ee75a561ba7e4dbc59355bcc1734c3
Björn Nilsved <nilsved@gmail.com>
2018-12-13 10:33:22 +0100

    Format code

--- tests/Feature/FormViewTests/InputTest.php
+++ tests/Feature/FormViewTests/InputTest.php
@@ -10,21 +10,31 @@ class InputTest extends IntegrationTest
 {
     public function test_input_type_defaults_to_text()
     {
-        $output = View::make('kontour::forms.input', ['name' => 'test', 'errors' => new MessageBag])->render();
+        $output = View::make('kontour::forms.input', [
+            'name' => 'test',
+            'errors' => new MessageBag,
+        ])->render();
 
         $this->assertRegExp('/<input[\S\s]*type="text"[\S\s]*>/', $output);
     }
 
     public function test_input_type_can_be_specified()
     {
-        $output = View::make('kontour::forms.input', ['name' => 'test', 'errors' => new MessageBag, 'type' => 'email'])->render();
+        $output = View::make('kontour::forms.input', [
+            'name' => 'test',
+            'errors' => new MessageBag,
+            'type' => 'email',
+        ])->render();
 
         $this->assertRegExp('/<input[\S\s]*type="email"[\S\s]*>/', $output);
     }
 
     public function test_input_is_referenced_by_label()
     {
-        $output = View::make('kontour::forms.input', ['name' => 'test', 'errors' => new MessageBag])->render();
+        $output = View::make('kontour::forms.input', [
+            'name' => 'test',
+            'errors' => new MessageBag,
+        ])->render();
 
         $this->assertRegExp('/<label\s*for="test"\s*>/', $output);
         $this->assertRegExp('/<input[\S\s]*id="test"\s*>/', $output);
@@ -32,7 +42,11 @@ class InputTest extends IntegrationTest
 
     public function test_input_can_have_custom_control_id()
     {
-        $output = View::make('kontour::forms.input', ['name' => 'test', 'errors' => new MessageBag, 'controlId' => 'a'])->render();
+        $output = View::make('kontour::forms.input', [
+            'name' => 'test',
+            'errors' => new MessageBag,
+            'controlId' => 'a',
+        ])->render();
 
         $this->assertRegExp('/<label\s*for="a"\s*>/', $output);
         $this->assertRegExp('/<input[\S\s]*id="a"\s*>/', $output);
@@ -40,7 +54,11 @@ class InputTest extends IntegrationTest
 
     public function test_input_can_have_id_prefix()
     {
-        $output = View::make('kontour::forms.input', ['name' => 'test', 'errors' => new MessageBag, 'idPrefix' => 'pre-'])->render();
+        $output = View::make('kontour::forms.input', [
+            'name' => 'test',
+            'errors' => new MessageBag,
+            'idPrefix' => 'pre-',
+        ])->render();
 
         $this->assertRegExp('/<label\s*for="pre-test"\s*>/', $output);
         $this->assertRegExp('/<input[\S\s]*id="pre-test"\s*>/', $output);
@@ -48,7 +66,12 @@ class InputTest extends IntegrationTest
 
     public function test_value_attribute_is_not_present_on_password_inputs()
     {
-        $output = View::make('kontour::forms.input', ['name' => 'test', 'errors' => new MessageBag, 'type' => 'password', 'value' => 'secret'])->render();
+        $output = View::make('kontour::forms.input', [
+            'name' => 'test',
+            'errors' => new MessageBag,
+            'type' => 'password',
+            'value' => 'secret',
+        ])->render();
 
         $this->assertRegExp('/<input[\S\s]*type="password"[\S\s]*>/', $output);
         $this->assertNotRegExp('/<input[\S\s]*value="[^"]*"[\S\s]*>/', $output);
@@ -56,7 +79,10 @@ class InputTest extends IntegrationTest
 
     public function test_default_value_is_empty_string()
     {
-        $output = View::make('kontour::forms.input', ['name' => 'test', 'errors' => new MessageBag])->render();
+        $output = View::make('kontour::forms.input', [
+            'name' => 'test',
+            'errors' => new MessageBag,
+        ])->render();
 
         $this->assertRegExp('/<input[\S\s]*value=""[\S\s]*>/', $output);
     }
@@ -65,16 +91,32 @@ class InputTest extends IntegrationTest
     {
         $this->withSession(['_old_input' => ['test' => 'old']]);
         request()->setLaravelSession(session());
-        $output = View::make('kontour::forms.input', ['name' => 'test', 'errors' => new MessageBag])->render();
+        $output = View::make('kontour::forms.input', [
+            'name' => 'test',
+            'errors' => new MessageBag,
+        ])->render();
 
         $this->assertRegExp('/<input[\S\s]*value="old"[\S\s]*>/', $output);
     }
 
     public function test_fallback_values_are_used_in_order()
     {
-        $fallbacks = ['value' => 'a', 'slot' => 'b', 'model' => ['test' => 'c']];
+        $fallbacks = [
+            'value' => 'a',
+            'slot' => 'b',
+            'model' => ['test' => 'c'],
+        ];
+
         while (count($fallbacks)) {
-            $output = View::make('kontour::forms.input', array_merge(['name' => 'test', 'errors' => new MessageBag], $fallbacks))->render();
+            $output = View::make('kontour::forms.input',
+                array_merge(
+                    [
+                        'name' => 'test',
+                        'errors' => new MessageBag,
+                    ],
+                    $fallbacks
+                )
+            )->render();
 
             $value = array_shift($fallbacks);
             if (is_array($value)) {
