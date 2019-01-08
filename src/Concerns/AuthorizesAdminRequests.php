bcbb8f774bf5981f77d905bbcb80420b7d2bd2c0
Björn Nilsved <nilsved@gmail.com>
2019-01-08 12:01:52 +0100

    Format code

--- src/Concerns/AuthorizesAdminRequests.php
+++ src/Concerns/AuthorizesAdminRequests.php
@@ -18,8 +18,12 @@ trait AuthorizesAdminRequests
     /**
      * Authorize the current request with the admin user, and dispatch a ShowAdminVisit event.
      */
-    public function authorizeShowAdminVisit($ability, string $linkName, $abilityArguments = [], string $linkDescription = null): AdminVisit
-    {
+    public function authorizeShowAdminVisit(
+        $ability,
+        string $linkName,
+        $abilityArguments = [],
+        string $linkDescription = null
+    ): AdminVisit {
         $link = $this->authorizeAdminVisit($ability, $linkName, $abilityArguments, $linkDescription);
         $visit = new ShowAdminVisit($link, $this->adminUser());
         event(new AdminToolVisited($visit));
@@ -30,9 +34,12 @@ trait AuthorizesAdminRequests
     /**
      * Authorize the current request with the admin user, and dispatch a EditAdminVisit event.
      */
-
-    public function authorizeEditAdminVisit($ability, string $linkName, $abilityArguments = [], string $linkDescription = null): AdminVisit
-    {
+    public function authorizeEditAdminVisit(
+        $ability,
+        string $linkName,
+        $abilityArguments = [],
+        string $linkDescription = null
+    ): AdminVisit {
         $link = $this->authorizeAdminVisit($ability, $linkName, $abilityArguments, $linkDescription);
         $visit = new EditAdminVisit($link, $this->adminUser());
         event(new AdminToolVisited($visit));
@@ -40,8 +47,12 @@ trait AuthorizesAdminRequests
         return $visit;
     }
 
-    protected function authorizeAdminVisit($ability, string $linkName, $abilityArguments = [], string $linkDescription = null): AdminLink
-    {
+    protected function authorizeAdminVisit(
+        $ability,
+        string $linkName,
+        $abilityArguments = [],
+        string $linkDescription = null
+    ): AdminLink {
         [$ability, $abilityArguments] = $this->parseAbilityAndArguments($ability, $abilityArguments);
 
         $result = $this->authorizeForUser($this->adminUser(), $ability, $abilityArguments);
