0ce4a750205c7c884bc9111d70eb08dfd9236196
Björn Nilsved <nilsved@gmail.com>
2019-01-08 11:12:27 +0100

    Create trait that authorizes requests and dispatches visit events

---
+++ src/Concerns/AuthorizesAdminRequests.php
@@ -0,0 +1,44 @@
+<?php
+
+namespace Kontenta\Kontour\Concerns;
+
+use Illuminate\Foundation\Auth\Access\AuthorizesRequests;
+use Kontenta\Kontour\AdminLink;
+use Kontenta\Kontour\Concerns\ResolvesAdminUser;
+use Kontenta\Kontour\Contracts\AdminVisit;
+use Kontenta\Kontour\EditAdminVisit;
+use Kontenta\Kontour\Events\AdminToolVisited;
+use Kontenta\Kontour\ShowAdminVisit;
+
+trait AuthorizesAdminRequests
+{
+    use AuthorizesRequests, ResolvesAdminUser;
+
+    public function authorizeShowAdminVisit($ability, string $linkName, $abilityArguments = [], string $linkDescription = null): AdminVisit
+    {
+        $link = $this->authorizeAdminVisit($ability, $linkName, $abilityArguments, $linkDescription);
+        $visit = new ShowAdminVisit($link, $this->adminUser());
+        event(new AdminToolVisited($visit));
+
+        return $visit;
+    }
+
+    public function authorizeEditAdminVisit($ability, string $linkName, $abilityArguments = [], string $linkDescription = null): AdminVisit
+    {
+        $link = $this->authorizeAdminVisit($ability, $linkName, $abilityArguments, $linkDescription);
+        $visit = new EditAdminVisit($link, $this->adminUser());
+        event(new AdminToolVisited($visit));
+
+        return $visit;
+    }
+
+    protected function authorizeAdminVisit($ability, string $linkName, $abilityArguments = [], string $linkDescription = null): AdminLink
+    {
+        [$ability, $abilityArguments] = $this->parseAbilityAndArguments($ability, $abilityArguments);
+
+        $result = $this->authorizeForUser($this->adminUser(), $ability, $abilityArguments);
+
+        return AdminLink::create($linkName, url()->full(), $linkDescription)
+            ->registerAbilityForAuthorization($ability, $abilityArguments);
+    }
+}
