39fc411ef64872b113dad102b002ab9b168636d9
Björn Nilsved <nilsved@gmail.com>
2019-01-11 15:54:15 +0100

    Add title to li if the link has no title
    This is to display the full string on hover if cut off by css width

--- resources/views/widgets/teamRecentVisits.blade.php
+++ resources/views/widgets/teamRecentVisits.blade.php
@@ -2,7 +2,7 @@
   <header>{{ trans('kontour::widgets.teamRecentVisits.title') }}</header>
   <ul>
   @foreach($visits as $visit)
-    <li data-kontour-visit-type="{{ $visit->getType() }}" data-kontour-username="{{ $visit->getUser()->getDisplayName() }}">{{ $visit->getLink() }}</li>
+    <li data-kontour-visit-type="{{ $visit->getType() }}" data-kontour-username="{{ $visit->getUser()->getDisplayName() }}"{!! empty($visit->getLink()->getDescription()) ? ' title="' . e($visit->getLink()->getName()) . '"' : '' !!}>{{ $visit->getLink() }}</li>
   @endforeach
   </ul>
 </aside>
