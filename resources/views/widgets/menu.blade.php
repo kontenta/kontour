345a6e1f4a8491198a896bc5dc6671a754e175fe
Björn Nilsved <nilsved@gmail.com>
2019-01-16 14:50:43 +0100

    Break out buttons

--- resources/views/widgets/menu.blade.php
+++ resources/views/widgets/menu.blade.php
@@ -1,8 +1,4 @@
-<button aria-expanded="true" hidden><svg viewBox="0 0 10 10" aria-label="{{ __('Menu') }}">
-  <rect class="top" height="2" width="8" y="1" x="1"/>
-  <rect class="middle" height="2" width="8" y="4" x="1"/>
-  <rect class="bottom" height="2" width="8" y="7" x="1"/>
-</svg></button>
+@include('kontour::buttons.hamburger')
 <ul data-kontour-widget="menu">
 @foreach($links as $heading => $headingLinks)
   @if(count($headingLinks))
