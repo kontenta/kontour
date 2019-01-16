4678f573da1d7acdede3b0852e7742d07706097d
Björn Nilsved <nilsved@gmail.com>
2019-01-16 15:45:06 +0100

    Put button title in the button for disabled style situations

--- resources/views/buttons/hamburger.blade.php
+++ resources/views/buttons/hamburger.blade.php
@@ -1,5 +1,8 @@
-<button aria-expanded="true" hidden><svg viewBox="0 0 10 10" aria-label="{{ __('Menu') }}">
-  <rect class="top" height="2" width="8" y="1" x="1"/>
-  <rect class="middle" height="2" width="8" y="4" x="1"/>
-  <rect class="bottom" height="2" width="8" y="7" x="1"/>
-</svg></button>
+<button aria-expanded="true" hidden>
+  <svg viewBox="0 0 10 10" aria-hidden="true">
+    <rect class="top" height="2" width="8" y="1" x="1"/>
+    <rect class="middle" height="2" width="8" y="4" x="1"/>
+    <rect class="bottom" height="2" width="8" y="7" x="1"/>
+  </svg>
+  <span class="sr-only">{{ __('Menu') }}</span>
+</button>
