6271b9a7421d6382020dc2f955ed0840c388d589
Björn Nilsved <nilsved@gmail.com>
2019-01-17 11:15:43 +0100

    Only show widgets having non-empty lists

--- resources/views/widgets/personalRecentVisits.blade.php
+++ resources/views/widgets/personalRecentVisits.blade.php
@@ -1,10 +1,12 @@
-<aside data-kontour-widget="personalRecentVisits">
-  <header>{{ trans('kontour::widgets.personalRecentVisits.title') }}</header>
-  <ul>
-  @foreach($visits as $visit)
-    @if(url()->full() != $visit->getLink()->getUrl())
-      <li data-kontour-visit-type="{{ $visit->getType() }}"{!! empty($visit->getLink()->getDescription()) ? ' title="' . e($visit->getLink()->getName()) . '"' : '' !!}>{{ $visit->getLink() }}</li>
-    @endif
-  @endforeach
-  </ul>
-</aside>
+@if(count($visits))
+  <aside data-kontour-widget="personalRecentVisits">
+    <header>{{ trans('kontour::widgets.personalRecentVisits.title') }}</header>
+    <ul>
+    @foreach($visits as $visit)
+      @if(url()->full() != $visit->getLink()->getUrl())
+        <li data-kontour-visit-type="{{ $visit->getType() }}"{!! empty($visit->getLink()->getDescription()) ? ' title="' . e($visit->getLink()->getName()) . '"' : '' !!}>{{ $visit->getLink() }}</li>
+      @endif
+    @endforeach
+    </ul>
+  </aside>
+@endif
--- resources/views/widgets/teamRecentVisits.blade.php
+++ resources/views/widgets/teamRecentVisits.blade.php
@@ -1,8 +1,10 @@
-<aside data-kontour-widget="teamRecentVisits">
-  <header>{{ trans('kontour::widgets.teamRecentVisits.title') }}</header>
-  <ul>
-  @foreach($visits as $visit)
-    <li data-kontour-visit-type="{{ $visit->getType() }}" data-kontour-username="{{ $visit->getUser()->getDisplayName() }}"{!! empty($visit->getLink()->getDescription()) ? ' title="' . e($visit->getLink()->getName()) . '"' : '' !!}>{{ $visit->getLink() }}</li>
-  @endforeach
-  </ul>
-</aside>
+@if(count($visits))
+  <aside data-kontour-widget="teamRecentVisits">
+    <header>{{ trans('kontour::widgets.teamRecentVisits.title') }}</header>
+    <ul>
+    @foreach($visits as $visit)
+      <li data-kontour-visit-type="{{ $visit->getType() }}" data-kontour-username="{{ $visit->getUser()->getDisplayName() }}"{!! empty($visit->getLink()->getDescription()) ? ' title="' . e($visit->getLink()->getName()) . '"' : '' !!}>{{ $visit->getLink() }}</li>
+    @endforeach
+    </ul>
+  </aside>
+@endif
