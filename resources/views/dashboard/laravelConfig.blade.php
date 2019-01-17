7d504eb2c2f73e9754bfa43e1f11d9d113f51461
Björn Nilsved <nilsved@gmail.com>
2019-01-17 11:07:48 +0100

    Add column utility classes

--- resources/views/dashboard/laravelConfig.blade.php
+++ resources/views/dashboard/laravelConfig.blade.php
@@ -2,7 +2,7 @@
   <header>
     Laravel configuration
   </header>
-  <dl>
+  <dl class="columns-narrow">
     @foreach(['app.name', 'app.url', 'app.env', 'app.debug', 'mail.from.address', 'mail.from.name', 'kontour.guard', 'session.cookie', 'session.lifetime', 'database.default', 'cache.default', 'filesystems.default'] as $configKey)
     <dt><code>{{ $configKey }}</code></dt>
     <dd>{{ config($configKey) }}</dd>
