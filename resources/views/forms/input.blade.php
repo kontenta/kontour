bd6a980a7402e566da1b67b0898063c228c1c8a8
Björn Nilsved <nilsved@gmail.com>
2019-01-25 08:24:58 +0100

    Move input element to elements directory

--- resources/views/forms/input.blade.php
+++ resources/views/forms/input.blade.php
@@ -1,7 +1,7 @@
 <div>
   @include('kontour::forms.label', ['controlId' => $controlId = $controlId ?? (($idPrefix ?? '') . $name)])
   <div>
-    @include('kontour::forms.partials.input', ['errorsId' => $errorsId = $controlId . ($errorsSuffix ?? 'Errors')])
+    @include('kontour::forms.elements.input', ['errorsId' => $errorsId = $controlId . ($errorsSuffix ?? 'Errors')])
     @include('kontour::forms.partials.errors')
   </div>
 </div>
