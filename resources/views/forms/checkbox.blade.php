dc3c37563674026e0446063f2fe860f0ff666d6b
Björn Nilsved <nilsved@gmail.com>
2019-01-25 08:09:05 +0100

    Move checkbox element to elements directory

--- resources/views/forms/checkbox.blade.php
+++ resources/views/forms/checkbox.blade.php
@@ -1,7 +1,7 @@
 <div>
   @component('kontour::forms.label', ['name' => $name, 'label' => $label ?? null, 'controlId' => $controlId = $controlId ?? (($idPrefix ?? '') . $name), 'labelAttributes' => $labelAttributes ?? []])
     @slot('labelStart')
-      @include('kontour::forms.partials.checkbox', ['errorsId' => $errorsId = $controlId . ($errorsSuffix ?? 'Errors')])
+      @include('kontour::forms.elements.checkbox', ['errorsId' => $errorsId = $controlId . ($errorsSuffix ?? 'Errors')])
     @endslot
   @endcomponent
   @include('kontour::forms.partials.errors')
