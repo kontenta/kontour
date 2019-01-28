ee40f477b6da01be5532ccbc8690b01b9653d5b5
Björn Nilsved <nilsved@gmail.com>
2019-01-28 09:42:24 +0100

    Explicitly pass legendAttributes to legend instead of labelAttributes

--- resources/views/forms/groups/checkboxes.blade.php
+++ resources/views/forms/groups/checkboxes.blade.php
@@ -2,7 +2,7 @@
   data-checked-checkboxes="{{ implode(' ', $selected = collect(old($name, $selected ?? $model[$name] ?? []))->map(function($item) { return strval($item instanceof Illuminate\Database\Eloquent\Model ? $item->getKey() : $item); })->all()) }}"
   @include('kontour::forms.partials.groupAttributes')
 >
-  @include('kontour::forms.elements.label', ['labelTag' => 'legend', 'errorsId' => $errorsId = $groupId . ($errorsSuffix ?? 'Errors')])
+  @include('kontour::forms.elements.label', ['labelTag' => 'legend', 'errorsId' => $errorsId = $groupId . ($errorsSuffix ?? 'Errors'), 'labelAttributes' => $legendAttributes ?? []])
   <input type="hidden" name="{{ $name }}" value="">
   @include('kontour::forms.partials.checkableOptions')
   @include('kontour::forms.partials.errors')
