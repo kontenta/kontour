246d0e5749798d2e28e150ad47178ed719f47663
Björn Nilsved <nilsved@gmail.com>
2019-02-06 11:13:04 +0100

    Add script for confirming item deletion

---
+++ resources/js/confirm-delete.js
@@ -0,0 +1,13 @@
+document.body.addEventListener("submit", function(event) {
+  if (event.target.querySelector('input[name="_method"][value="delete"]')) {
+    let message = "Delete item";
+    let button = event.target.querySelector('[type="submit"]');
+    if (button) {
+      message = button.getAttribute("aria-label") || button.innerText || message;
+    }
+
+    if (!confirm(message)) {
+      event.preventDefault();
+    }
+  }
+});
