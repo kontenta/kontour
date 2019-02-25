fd896a0d107cf6668c2b5e33ad3de7acc009dea2
Björn Nilsved <nilsved@gmail.com>
2019-02-25 17:13:55 +0100

    Move the attribute template to be used not only by form templates

--- resources/views/forms/elements/label.blade.php
+++ resources/views/forms/elements/label.blade.php
@@ -4,7 +4,7 @@
 @endif
 @if(isset($labelAttributes) and is_iterable($labelAttributes))
   @foreach($labelAttributes as $attributeName => $attributeValue)
-    @include('kontour::forms.partials.attribute')
+    @include('kontour::partials.attribute')
   @endforeach
 @endif
 >{{ $labelStart ?? '' }}{{ $label ?? ucfirst(Lang::has('validation.attributes.'.$name) ? Lang::trans('validation.attributes.' . $name) : str_replace(['_', '.', '[]'], [' ', ' ', ''], $name)) }}{{ $labelEnd ?? '' }}</{{ $labelTag }}>
