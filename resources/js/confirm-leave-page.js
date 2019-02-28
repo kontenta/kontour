fee8caa0e34df5158014e0b92cc0f24155e5ed1f
Björn Nilsved <nilsved@gmail.com>
2019-02-28 16:44:41 +0100

    Mark inputs as "saving" instead of removing the beforeunload handler

--- resources/js/confirm-leave-page.js
+++ resources/js/confirm-leave-page.js
@@ -1,18 +1,24 @@
 (function() {
+  // Mark inputs as dirty when they change
   document.body.addEventListener("change", function(event) {
     event.target.setAttribute("data-kontour-dirty", "true");
   });
 
-  function kontourConfirmBeforeunload(event) {
+  // Mark dirty inputs as saving when their form is submitted
+  // If you submit via ajax, you may want to set them back to dirty again if the request fails
+  document.body.addEventListener("submit", function(event) {
+    event.target
+      .querySelectorAll('[data-kontour-dirty="true"]')
+      .forEach(element => {
+        element.setAttribute("data-kontour-dirty", "saving");
+      });
+  });
+
+  // Alert user if any inputs on the page are dirty before leaving
+  window.addEventListener("beforeunload", function(event) {
     if (document.querySelector('[data-kontour-dirty="true"]')) {
       event.preventDefault();
       event.returnValue = "You have unsaved changes.";
     }
-  }
-
-  window.addEventListener("beforeunload", kontourConfirmBeforeunload);
-
-  document.body.addEventListener("submit", function(event) {
-    window.removeEventListener("beforeunload", kontourConfirmBeforeunload);
   });
 })();
