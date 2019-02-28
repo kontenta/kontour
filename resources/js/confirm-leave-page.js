cce342acff76efbef336965c9132387c9b552717
Björn Nilsved <nilsved@gmail.com>
2019-02-28 16:09:54 +0100

    Remove the listener when a form is submitted

--- resources/js/confirm-leave-page.js
+++ resources/js/confirm-leave-page.js
@@ -1,10 +1,18 @@
-window.addEventListener("beforeunload", function(event) {
-  if (document.querySelector('[data-kontour-dirty="true"]')) {
-    event.preventDefault();
-    event.returnValue = "You have unsaved changes.";
-  }
-});
+(function() {
+  document.body.addEventListener("change", function(event) {
+    event.target.setAttribute("data-kontour-dirty", "true");
+  });
 
-document.body.addEventListener("change", function(event) {
-  event.target.setAttribute("data-kontour-dirty", "true");
-});
+  function kontourConfirmBeforeunload(event) {
+    if (document.querySelector('[data-kontour-dirty="true"]')) {
+      event.preventDefault();
+      event.returnValue = "You have unsaved changes.";
+    }
+  }
+
+  window.addEventListener("beforeunload", kontourConfirmBeforeunload);
+
+  document.body.addEventListener("submit", function(event) {
+    window.removeEventListener("beforeunload", kontourConfirmBeforeunload);
+  });
+})();
