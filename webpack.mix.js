fa6428ea8fd72b48777ecbe753c0a36e87170246
Björn Nilsved <nilsved@gmail.com>
2019-02-28 16:46:42 +0100

    Include confirm-leave script in build

--- webpack.mix.js
+++ webpack.mix.js
@@ -2,4 +2,7 @@ let mix = require("laravel-mix");
 
 mix
   .sass("resources/sass/kontour.scss", "resources/css/kontour.css")
-  .babel(["resources/js/confirm-delete.js"], "resources/js/kontour.js");
+  .babel(
+    ["resources/js/confirm-delete.js", "resources/js/confirm-leave-page.js"],
+    "resources/js/kontour.js"
+  );
