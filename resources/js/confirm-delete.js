a1391e762d3e4beb3e3cc0a7fbecab31e851b3a7
Björn Nilsved <nilsved@gmail.com>
2019-04-04 09:59:06 +0200

    Take method and button from forms elements
    ...accounting for FormData support and the form having multiple submit buttons.

--- resources/js/confirm-delete.js
+++ resources/js/confirm-delete.js
@@ -1,19 +1,45 @@
+// Show confirm dialog before submitting any spoofed DELETE method form
 document.body.addEventListener("submit", function(event) {
-  let formData = new FormData(event.target);
-  let method = (formData.getAll('_method').pop() || event.target.method).toUpperCase();
-  let button;
+  let method;
+  let deleteButton;
+
+  // Take last spoofed method using FormData if available
+  if (FormData instanceof Object) {
+    let formData = new FormData(event.target);
+    if (typeof formData.getAll == "function") {
+      method = (
+        formData.getAll("_method").pop() || event.target.method
+      ).toUpperCase();
+    }
+  }
 
   [...event.target.elements].forEach(function(input) {
-    if(input.type == 'submit') {
-      button = input;
+    // Take first spoofed method if FormData was not available
+    if (!method && input.name == "_method") {
+      method = input.value.toUpperCase();
+    }
+    // Take first submit button
+    if (!deleteButton && input.type == "submit") {
+      deleteButton = input;
+    }
+    // Take last DELETE button over first button
+    if (
+      input.type == "submit" &&
+      input.name == "_method" &&
+      input.value.toUpperCase() == "DELETE"
+    ) {
+      deleteButton = input;
     }
   });
 
-  if (method == 'DELETE') {
+  if (method == "DELETE") {
     let message = "Delete item";
-    if (button) {
+    if (deleteButton) {
       message =
-        button.getAttribute("aria-label") || button.title || button.innerText || message;
+        deleteButton.getAttribute("aria-label") ||
+        deleteButton.title ||
+        deleteButton.innerText ||
+        message;
     }
 
     if (!confirm(message)) {
