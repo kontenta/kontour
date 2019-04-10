8554d9e4dcac737bfca1c7af1c39672035e4c66f
Björn Nilsved <nilsved@gmail.com>
2019-04-10 12:05:01 +0200

    Check all elements belonging to form
    ...not just the ones within the form tag.
    
    Closes #112

--- resources/js/confirm-leave-page.js
+++ resources/js/confirm-leave-page.js
@@ -6,13 +6,13 @@
 });
 
 // Mark dirty inputs as saving when their form is submitted
-// If you submit via ajax, you may want to set them back to dirty again if the request fails
+// If you submit via ajax, you will want to removeAttribute("data-kontour-dirty") if the request succeedes.
 document.body.addEventListener("submit", function(event) {
-  event.target
-    .querySelectorAll('[data-kontour-dirty="true"]')
-    .forEach(function(element) {
-      element.setAttribute("data-kontour-dirty", "saving");
-    });
+  [...event.target.elements].forEach(function(input) {
+    if (input.getAttribute("data-kontour-dirty") == "true") {
+      input.setAttribute("data-kontour-dirty", "saving");
+    }
+  });
 });
 
 // Alert user if any inputs on the page are dirty before leaving
