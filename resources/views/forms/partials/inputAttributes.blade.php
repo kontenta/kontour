7ae629a499a2d37acabd357c460167da6ff3fafd
Björn Nilsved <nilsved@gmail.com>
2019-04-12 17:12:58 +0200

    Add placeholder attribute to inputs

--- resources/views/forms/partials/inputAttributes.blade.php
+++ resources/views/forms/partials/inputAttributes.blade.php
@@ -9,6 +9,9 @@ id="{{ $controlId }}"
 @if(!empty($autofocusControlId) and $autofocusControlId == $controlId)
   autofocus
 @endif
+@if(!empty($placeholder))
+  placeholder="{{ $placeholder }}"
+@endif
 @if(isset($controlAttributes) and is_iterable($controlAttributes))
   @foreach($controlAttributes as $attributeName => $attributeValue)
     @include('kontour::partials.attribute')
