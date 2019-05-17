79f6d2e50eb175adb5d84ee393492be7f3dc1477
Björn Nilsved <nilsved@gmail.com>
2019-05-17 10:04:16 +0200

    Ensure multiselects work with dot-notation names
    Closes #115

--- resources/views/forms/groups/multiselect.blade.php
+++ resources/views/forms/groups/multiselect.blade.php
@@ -2,7 +2,7 @@
   data-selected-options="{{ implode(' ', $selected = collect(($errors->any() ? old($name) : null) ?? $selected ?? $model[$name] ?? [])->map(function($item) { return strval($item instanceof Illuminate\Database\Eloquent\Model ? $item->getKey() : $item); })->all()) }}"
   @include('kontour::forms.partials.groupAttributes')
 >
-  <input type="hidden" name="{{ $name }}" value="">
+  <input type="hidden" @include('kontour::forms.partials.nameAttribute') value="">
   @include('kontour::forms.label', [
     'controlId' => $controlId = $controlId ?? (($idPrefix ?? '') . $name),
   ])
