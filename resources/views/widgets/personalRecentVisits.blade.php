b2e47e34e543d8585542f5a9a53a399f2a612f46
Björn Nilsved <nilsved@gmail.com>
2020-05-01 16:38:09 +0200

    Use role="list" to keep list-semantics in safari
    https://www.scottohara.me/blog/2019/01/12/lists-and-safari.html#a-new-alternative
    
    Closes #142

--- resources/views/widgets/personalRecentVisits.blade.php
+++ resources/views/widgets/personalRecentVisits.blade.php
@@ -1,7 +1,7 @@
 @if(count($visits))
   <aside data-kontour-widget="personalRecentVisits">
     <header>{{ trans('kontour::widgets.personalRecentVisits.title') }}</header>
-    <ul>
+    <ul role="list">
     @foreach($visits as $visit)
       @if(url()->full() != $visit->getLink()->getUrl())
         <li data-kontour-visit-type="{{ $visit->getType() }}"{!! empty($visit->getLink()->getDescription()) ? ' title="' . e($visit->getLink()->getName()) . '"' : '' !!}>{{ $visit->getLink() }}</li>
