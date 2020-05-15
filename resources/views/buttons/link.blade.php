cb884e6c640c0e24d368f3ea8beee60b20a8d8df
Björn Nilsved <nilsved@gmail.com>
2020-05-15 17:34:45 +0200

    Remove title from link

--- resources/views/buttons/link.blade.php
+++ resources/views/buttons/link.blade.php
@@ -1,7 +1,4 @@
 <a href="{{ $href }}"
-  @if(isset($description))
-    title="{{ $description }}"
-  @endif
   data-kontour-button="link"
   @include('kontour::buttons.partials.buttonAttributes')
 >{{ $slot ?? $description }}</a>
