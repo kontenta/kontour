1416c18fa09193df368a4caa9367686fe7eec864
Björn Nilsved <nilsved@gmail.com>
2020-06-12 12:39:32 +0200

    Make text small in item history widget

--- resources/views/widgets/itemHistory.blade.php
+++ resources/views/widgets/itemHistory.blade.php
@@ -3,11 +3,13 @@
   <ul role="list">
   @foreach($entries as $entry)
     <li lang="en" data-kontour-entry-action="{{ $entry['action'] }}"@if($entry['user']) data-kontour-username="{{ $entry['user']->getDisplayName() }}"@endif>
+      <small>
         <span>{{ ucfirst($entry['action']) }}</span>
         @include('kontour::elements.time', ['carbon' => $entry['datetime']])
         @if($entry['user'])
             <span>by</span> <span>{{ $entry['user']->getDisplayName() }}</span>
         @endif
+      </small>
     </li>
   @endforeach
   </ul>
