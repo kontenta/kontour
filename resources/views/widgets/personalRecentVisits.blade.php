50357e212218ebd011dd220ae643d9084a6bc034
Björn Nilsved <nilsved@gmail.com>
2020-06-12 13:07:53 +0200

    Fix formatting for tests

--- resources/views/widgets/personalRecentVisits.blade.php
+++ resources/views/widgets/personalRecentVisits.blade.php
@@ -4,9 +4,7 @@
     <ul role="list">
     @foreach($visits as $visit)
       @if(url()->full() != $visit->getLink()->getUrl())
-        <li data-kontour-visit-type="{{ $visit->getType() }}"{!! empty($visit->getLink()->getDescription()) ? ' title="' . e($visit->getLink()->getName()) . '"' : '' !!}>
-          <small>{{ $visit->getLink() }}</small>
-        </li>
+        <li data-kontour-visit-type="{{ $visit->getType() }}"{!! empty($visit->getLink()->getDescription()) ? ' title="' . e($visit->getLink()->getName()) . '"' : '' !!}><small>{{ $visit->getLink() }}</small></li>
       @endif
     @endforeach
     </ul>
--- resources/views/widgets/teamRecentVisits.blade.php
+++ resources/views/widgets/teamRecentVisits.blade.php
@@ -3,9 +3,7 @@
     <header>{{ trans('kontour::widgets.teamRecentVisits.title') }}</header>
     <ul role="list">
     @foreach($visits as $visit)
-      <li data-kontour-visit-type="{{ $visit->getType() }}" data-kontour-username="{{ $visit->getUser()->getDisplayName() }}"{!! empty($visit->getLink()->getDescription()) ? ' title="' . e($visit->getLink()->getName()) . '"' : '' !!}>
-        <small>{{ $visit->getLink() }}</small>
-      </li>
+      <li data-kontour-visit-type="{{ $visit->getType() }}" data-kontour-username="{{ $visit->getUser()->getDisplayName() }}"{!! empty($visit->getLink()->getDescription()) ? ' title="' . e($visit->getLink()->getName()) . '"' : '' !!}><small>{{ $visit->getLink() }}</small></li>
     @endforeach
     </ul>
   </aside>
