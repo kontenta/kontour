a4aa2af9d16c7ae1d1e45e1f5ac17bba80e722b9
Björn Nilsved <nilsved@gmail.com>
2020-06-12 14:09:44 +0200

    Add example radiobuttons

--- tests/Feature/Fakes/resources/views/edit.blade.php
+++ tests/Feature/Fakes/resources/views/edit.blade.php
@@ -5,6 +5,8 @@
 @section('kontourToolMain')
 <form>
   @include('kontour::forms.input', ['name' => 'test', 'value' => 'An example'])
+  @include('kontour::forms.radiobuttons', ['name' => 'options', 'label' => 'Some options',
+  'options' => ['This is a choice you can make', 'This is another', 'Last chance here...']])
   <div data-kontour-section="kontourStickyActions">
     <div class="cluster">
       <div>
