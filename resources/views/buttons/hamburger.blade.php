6886883564f02e60ac403769342793f7fd6103d3
Björn Nilsved <nilsved@gmail.com>
2020-09-10 10:57:26 +0200

    Add two-word verbs for actions
    This is to avoid conflicts with userland translation files with single-word names.
    See https://github.com/kontenta/kontour/issues/178
    
    Closes #191

--- resources/views/buttons/hamburger.blade.php
+++ resources/views/buttons/hamburger.blade.php
@@ -6,5 +6,5 @@
     <rect class="middle" height="2" width="8" y="4" x="1"/>
     <rect class="bottom" height="2" width="8" y="7" x="1"/>
   </svg>
-  <span class="sr-only">{{ __('Menu') }}</span>
+  <span class="sr-only">{{ __('Open menu') }}</span>
 </button>
